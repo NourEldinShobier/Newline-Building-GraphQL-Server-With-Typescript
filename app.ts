0e2fc0fa41c4eea97bebb27b49e70994f80f0aa0
Nour <nour.eldin.shobier@gmail.com>
2020-08-11 12:34:25 +0200

    Create POST/GET routes

--- app.ts
+++ app.ts
@@ -1,13 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { list } from "./src/lists";
 
 const app = express();
 const port = 9000;
 
+app.use(bodyParser.json());
 
-const one: number = 1;
-const two: any = 2;
+app.get("/", (_req, res) => res.send('It\'s Working'));
+app.get("/list", (_req, res) => res.send(list));
 
-app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
+
+app.post('/delete-list', (req, res) => {
+    const id: string = req.body.id;
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].id === id) {
+            return res.send(list.splice(i, 1));
+        }
+    }
+
+    return res.send("failed to deleted listing");
+});
+
+
+
+
 app.listen(port);
 
 console.log(`[server]: http://localhost:${port}`);

8e401a621c262dc4c1ff999f9c60828d434241cf
Nour <nour.eldin.shobier@gmail.com>
2020-08-11 10:45:05 +0200

    Module (1)

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+
+const one: number = 1;
+const two: number = 2;
+
+app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
+app.listen(port);
+
+console.log(`[server]: http://localhost:${port}`);
